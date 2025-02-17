e2ee49e923a69284de27bb29dbe30d0953fe8c69
ahmedelsayed3496 <92544401+ahmedelsayed3496@users.noreply.github.com>
2025-02-17 22:55:34 +0200

    Add project files.

---
+++ Hospital/Controllers/DoctorsController.cs
@@ -0,0 +1,61 @@
+﻿using Hospital.DataAccess;
+using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BookAppointment()
+        {
+            var doctors = dbContext.Doctors;
+            return View(doctors.ToList());
+        }
+        [HttpGet]
+        public IActionResult CompleteAppointment(int id)
+        {
+            var doctor = dbContext.Doctors.FirstOrDefault(e => e.Id == id);
+            if (doctor == null)
+            {
+                return RedirectToAction("NotFoundPage");
+            }
+
+            return View(doctor);
+        }
+
+        [HttpPost]
+        public IActionResult CompleteAppointment(string patientName, DateTime appointmentDate, TimeSpan appointmentTime, int doctorId)
+        {
+            var appointment = new Appointment
+            {
+                PatientName = patientName,
+                AppointmentDate = appointmentDate,
+                AppointmentTime = appointmentTime,
+                DoctorId = doctorId
+            };
+
+            dbContext.Appointments.Add(appointment);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("ReservationsManagement");
+        }
+        public IActionResult ReservationsManagement()
+        {          
+            var appointments = dbContext.Appointments.Include(e => e.Doctor);
+
+            return View(appointments.ToList());
+        }
+
+        public IActionResult NotFoundPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hospital/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Models;
+
+namespace ContactDoctor.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public string Img { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
